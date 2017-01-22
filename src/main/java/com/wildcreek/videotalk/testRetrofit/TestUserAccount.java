37ef1d2cf83fad84919a4d48764367bb26f7703a
wildcreek <yujihu989@163.com>
2017-01-22 12:22:20 +0800

    添加 Retrofit 测试类

--- src/main/java/com/wildcreek/videotalk/testRetrofit/TestUserAccount.java
+++ src/main/java/com/wildcreek/videotalk/testRetrofit/TestUserAccount.java
@@ -30,7 +30,7 @@ public class TestUserAccount {
             .build();
     static UserApi userApi = retrofit.create(UserApi.class);
     public static void main(String args[]) {
-        testLogin();
+        login("18903769013","12345");
     }
 
     public interface UserApi{
@@ -67,45 +67,30 @@ public class TestUserAccount {
                 .subscribe(observer);
     }
 
-    public static void login(String username, String password,Observer<ResultModelTest<LoginResponse>> observer){
+    public static void login(String username, String password){
         HashMap<String,String> params = new HashMap<>();
         params.put("clientID","11222233");
         params.put("userAccount",username);
         params.put("password",password);
         RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"),new Gson().toJson(params));
-        setSubscribe(userApi.login(body),observer);
-    }
+        userApi.login(body)
+                .subscribeOn(Schedulers.immediate())
+                .subscribe(new Observer<ResultModelTest<LoginResponse>>() {
+                    @Override
+                    public void onCompleted() {
+                        System.out.println("登录返回onCompleted：");
+                    }
 
-    public static void testLogin(){
-//        Call<ResultModelTest<LoginResponse>> call = userApi.login(body);
-//        call.enqueue(new Callback<ResultModelTest<LoginResponse>>() {
-//            @Override
-//            public void onResponse(Call<ResultModelTest<LoginResponse>> call, Response<ResultModelTest<LoginResponse>> response) {
-//                System.out.println("登录返回结果：" + response.code() + response.message() + ((LoginResponse)response.body().getContent()).userToken);
-//
-//            }
-//
-//            @Override
-//            public void onFailure(Call<ResultModelTest<LoginResponse>> call, Throwable throwable) {
-//                System.out.println("登录返回失败：" + throwable.toString());
-//            }
-//        });
-        TestUserAccount.login("18903769013", "12345", new Observer<ResultModelTest<LoginResponse>>() {
-            @Override
-            public void onCompleted() {
-                System.out.println("登录返回onCompleted：");
-            }
+                    @Override
+                    public void onError(Throwable throwable) {
+                        System.out.println("登录返回失败：" + throwable.toString());
+                    }
 
-            @Override
-            public void onError(Throwable throwable) {
-                System.out.println("登录返回失败：" + throwable.toString());
-            }
-
-            @Override
-            public void onNext(ResultModelTest<LoginResponse> response) {
-                System.out.println("登录返回结果：" + response );
-            }
-        });
+                    @Override
+                    public void onNext(ResultModelTest<LoginResponse> response) {
+                        System.out.println("登录返回结果：" + response );
+                    }
+                });
     }
 
 }
